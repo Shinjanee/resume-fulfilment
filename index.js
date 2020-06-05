6944c5220abab285cba431c9d7378a946312d6df
Shinjanee <shinjaneegupta@gmail.com>
2020-06-05 23:02:08 +0530

    create api

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const port = process.env.PORT;
+const app= express();
+app.use(express.urlencoded({extended:false}));
+app.post('/',function(req,res){
+   
+    return res.json(200,
+        {
+            'fulfillmentText':"12:00 pm"
+        });
+ })
+app.listen(port,function(err){
+    if(err){
+       console.log("Error in running server");
+    }
+    console.log("server started");
+})
