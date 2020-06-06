a1c2723eb05b51de0a3615eb92209737b59e52e0
Shinjanee <shinjaneegupta@gmail.com>
2020-06-06 20:02:57 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-
-
-  const https = require('https');
-
-  https.get("https://jobs.github.com/positions.json?page=1&search=code", (resp) => {
-  let data = '';
-
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data));
+const express = require('express');
+const port = process.env.PORT;
+const app= express();
+app.use(express.urlencoded({extended:false}));
+app.post('/',function(req,res){
+   
     return res.json(200,
         {
-            'fulfillmentText':JSON.parse(data)
+            'fulfillmentText':"12:00 pm"
         });
-  });
-
-  }).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-
-
-
+ })
+app.listen(port,function(err){
+    if(err){
+       console.log("Error in running server");
+    }
+    console.log("server started");
+})
