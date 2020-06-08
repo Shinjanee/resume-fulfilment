9632af7ed07d57a84fefb19ed4a7c39298a41219
Shinjanee <shinjaneegupta@gmail.com>
2020-06-08 14:27:21 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
-const express = require('express');
-const port = process.env.PORT;
-const app= express();
-var skill = ""
-const https = require('https');
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://mongo_user:VwvmBKZFmXARA5ME@cluster0-r121x.gcp.mongodb.net/chatbot?retryWrites=true&w=majority";
 
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://mongo_user:VwvmBKZFmXARA5ME@cluster0-r121x.gcp.mongodb.net/chatbot?retryWrites=true&w=majority";
 MongoClient.connect(uri, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
    }
    console.log('Connected...');
    const collection = client.db("chatbot").collection("user_details");
-   var myobj = { name: "hello", location: "India" };
+
+  //insert
+  var myobj = { name: "Company Inc", location: "Highway 37" };
   collection.insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
     client.close();
   });
+   // perform actions on the collection object
    client.close();
- });
-
-
-app.post('/',function(req,res){
-
-  skill = req.body.queryResult.parameters["skill_name"];
-
-  https.get("https://jobs.github.com/positions.json?description="+skill+"&location=new+york", (resp) => {
-  let data = '';
-
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  resp.on('end', () => {
-     console.log(skill+",");
-     console.log(JSON.parse(data));
-     var jobsArray =  JSON.parse(data);
-     var result="";
-     for(var i=0;i<jobsArray.length;i++)
-     {
-        result = result + " \n" + (i+1).toString() + " " + jobsArray[i].title + " and " + jobsArray[i].url + "\n";
-     }
-    return res.json(200,
-        {
-          "fulfillmentText": result
-            
-        });
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
 });
-})
-
-app.listen(port,function(err){
-    if(err){
-       console.log("Error in running server");
-    }
-    console.log("server started");
-})
