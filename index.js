9e149ac77b844b31721c9104677e7db67362c677
Shinjanee <shinjaneegupta@gmail.com>
2020-06-08 18:52:13 +0530

    update index.js

--- index.js
+++ index.js
@@ -38,22 +38,22 @@ app.post('/',function(req,res){
   console.log("Error: " + err.message);
 });
    
-   MongoClient.connect(uri, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-   const collection = client.db("chatbot").collection("user_details");
+//    MongoClient.connect(uri, function(err, client) {
+//    if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//    }
+//    console.log('Connected...');
+//    const collection = client.db("chatbot").collection("user_details");
 
-  //insert
-  var myobj = { name: skill, location: "Highway 37" };
-  collection.insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    client.close();
-  });
-   client.close();
-});
+//   //insert
+//   var myobj = { name: skill, location: "Highway 37" };
+//   collection.insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     client.close();
+//   });
+//    client.close();
+// });
    
  })
 app.listen(port,function(err){
