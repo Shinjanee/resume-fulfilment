39091daf9ffbf7b8eaf24f8dd0ca7bb864b5793c
Shinjanee <shinjaneegupta@gmail.com>
2020-06-09 00:41:34 +0530

    update index.js

--- index.js
+++ index.js
@@ -35,22 +35,7 @@ app.post('/',function(req,res){
   {
       return res.json(200,
         {
-          //"fulfillmentText": user_name + "\n Education?"
-          "fulfillmentMessages": [
-              {
-                "card": {
-                  "title": "card title",
-                  "subtitle": "card text",
-                  "imageUri": "https://example.com/images/example.png",
-                  "buttons": [
-                    {
-                      "text": "button text",
-                      "postback": "https://example.com/path/for/end-user/to/follow"
-                    }
-                  ]
-                }
-              }
-            ]
+          "fulfillmentText": user_name + "\n Education?"
             
         });
   }
