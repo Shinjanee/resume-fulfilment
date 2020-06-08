bbb954609f99f412d8a53c88e8fd4d9fcdc7c4d8
Shinjanee <shinjaneegupta@gmail.com>
2020-06-08 15:22:11 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
-
 const uri = "mongodb+srv://mongo_user:VwvmBKZFmXARA5ME@cluster0-r121x.gcp.mongodb.net/chatbot?retryWrites=true&w=majority";
+
 MongoClient.connect(uri, function(err, client) {
    if(err) {
         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
