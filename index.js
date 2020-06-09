036481ae30e212b2dac9c1de69e70632c8e4a7b4
Shinjanee <shinjaneegupta@gmail.com>
2020-06-09 23:51:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -311,7 +311,7 @@ app.post('/',function(req,res){
            else if(val == "interests")
             toSend = user.interests;
            else if(val == "education")
-            toSend = user.education.degree + user.education.university + user.education.location + user.education.percentage;
+            toSend = String(user.education.degree) + String(user.education.university) + String(user.education.location) + String(user.education.percentage);
            else if (val == "projects")
             toSend = user.project;
            else if(val == "experience")
