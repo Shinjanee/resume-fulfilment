14908aaf7338026a346fe428c8c6fcfba4b6cc9c
Shinjanee <shinjaneegupta@gmail.com>
2020-06-11 23:45:14 +0530

    update index.js

--- index.js
+++ index.js
@@ -140,6 +140,7 @@ app.post('/', function(req, res) {
                 var main_str = user.skills;
                 var str = req.body.queryResult.queryText;
                 final_str = main_str.replace(str, "");
+                console.log(main_str + " " + final_str);
                 query = main_str.replace(", $", "");
             }
             User.findByIdAndUpdate(id, {
