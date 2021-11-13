81bad448eedab1463258dccf0e1798b7ea372db2
Venis13 <venispatel13@gmail.com>
2021-11-13 13:17:05 +0530

    change mongo config

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://mongo_user:VwvmBKZFmXARA5ME@cluster0-r121x.gcp.mongodb.net/chatbot?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://mongo_user:j0FSSp1FlEwuWnPi@cluster0-r121x.gcp.mongodb.net/chatbot?retryWrites=true&w=majority');
 //aqcuire the connection
 const db = mongoose.connection;
 //error
-db.on('error',console.error.bind(console,'error connectiveity to db'));
+db.on('error',console.error.bind(console,'error connecting to db'));
 //up and running tehn  print the message
 db.once('open',function(){
     console.log("Successfully connected to db");
